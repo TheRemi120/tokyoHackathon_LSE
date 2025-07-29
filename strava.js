3b91261a17c7a3ec409fbee4cdf495822d4b0060
Dwayne67E <56132463+Dwayne67E@users.noreply.github.com>
2025-07-30 08:50:47 +0900

    Add accès key

--- strava.js
+++ strava.js
@@ -1,6 +1,6 @@
 import fetch from 'node-fetch';
 
-const accessToken = 'ACCESS_TOKEN';
+const accessToken = 'fc76f757b1ebcb51252b707f1ff7a55b8bbab3c6';
 
 async function getActivities() {
   const res = await fetch('https://www.strava.com/api/v3/athlete/activities', {
